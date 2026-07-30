9aa03978d97804482ad15bd5343b775f2e039ba5
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-07-30 19:11:56 -0400

    Add color definitions for NexGo theme

---
+++ app/src/main/java/com/nexgo/iptv/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.nexgo.iptv.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NexGoBackground = Color(0xFF0B1220)
+val NexGoSurface = Color(0xFF131C2E)
+val NexGoPrimary = Color(0xFF2FD3E0)
+val NexGoPrimaryDark = Color(0xFF1AA5B0)
+val NexGoAccent = Color(0xFF7C5CFF)
+val NexGoText = Color(0xFFF5F7FA)
+val NexGoTextDim = Color(0xFF93A1B5)
